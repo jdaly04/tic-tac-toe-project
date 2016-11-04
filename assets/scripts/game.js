85cca03e0b6bcf238408ab6e5c83fd8e090fcd74
jdaly04 <julia.m.daly@gmail.com>
2016-11-03 22:36:44 -0400

    Add functionality for all win conditions
    Update gamejs branch with functioning win conditions.
    Using console.log right now for success confirmation.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,13 +5,39 @@ let board = ['', '', '', '', '', '', '', '', ''];
 let player = '';
 //let checkWin= function() {
 
-
+//CHECK WIN ROWS:
 
 let checkWin = function () {
-   if (board[0] === "X" && board[1] === "X" && board[2] === "X") {
-      console.log("x won");
-  }
+   if ((board[0] === "X" && board[1] === "X" && board[2] === "X") ||
+      (board[3] === "X" && board[4] === "X" && board[5] === "X")  ||
+      (board[6] === "X" && board[7] === "X" && board[8] === "X") ||
+      (board[0] === "X" && board[3] === "X" && board[6] === "X") ||
+      (board[1] === "X" && board[4] === "X" && board[7] === "X") ||
+      (board[2] === "X" && board[5] === "X" && board[8] === "X") ||
+      (board[0] === "X" && board[4] === "X" && board[8] === "X") ||
+      (board[2] === "X" && board[4] === "X" && board[6] === "X"))
+    {
+
+      console.log("X Won!");
+    }
+  if ((board[0] === "O" && board[1] === "O" && board[2] === "O") ||
+     (board[3] === "O" && board[4] === "O" && board[5] === "O")  ||
+     (board[6] === "O" && board[7] === "O" && board[8]=== "O") ||
+     (board[0] === "O" && board[3] === "O" && board[6] === "O") ||
+     (board[1] === "O" && board[4] === "O" && board[7] === "O") ||
+     (board[2] === "O" && board[5] === "O" && board[8] === "O") ||
+     (board[0] === "O" && board[4] === "O" && board[8] === "O") ||
+     (board[2] === "O" && board[4] === "O" && board[6] === "O"))
+  {
+    console.log("O Won!");
+
+  } else if ((board[0] !== "" && board[1] !== "" && board[2] && board[3]!== "" && board[4] !== "" && board[5] !== "" && board[6] !== "" &&
+            board[7] !== "" && board[8] !== "" && board[9] !== ""))
+{
+}
 };
+
+
 
 //let currentPlayer = function() {
   //if(player ==='1'){
